cb90288b0f2e0ee418993607200dc1240fd950ce
suinua <maatinosippo@gmail.com>
2021-04-04 01:33:04 +0900

    API層の作成

---
+++ src/game_assistant/GameAssistant.php
@@ -0,0 +1,83 @@
+<?php
+
+
+namespace game_assistant;
+
+
+use game_assistant\models\Game;
+use game_assistant\models\GameId;
+use game_assistant\models\TeamId;
+use game_assistant\services\GameService;
+use game_assistant\services\SoloGameService;
+use game_assistant\services\TeamGameService;
+use game_assistant\store\GamesStore;
+use pocketmine\plugin\PluginLogger;
+use pocketmine\scheduler\TaskScheduler;
+
+class GameAssistant
+{
+    static private PluginLogger $logger;
+    static private TaskScheduler $scheduler;
+
+    static function setLogger(PluginLogger $logger): void {
+        self::$logger = $logger;
+    }
+
+    public static function setScheduler(TaskScheduler $scheduler): void {
+        self::$scheduler = $scheduler;
+    }
+
+    static function registerGame(Game $game): bool {
+        try {
+            GameService::register($game);
+        } catch (\Exception $e) {
+            self::$logger->error($e->getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    static function startGame(GameId $gameId): bool {
+        try {
+            GameService::start($gameId, self::$scheduler);
+        } catch (\Exception $e) {
+            self::$logger->error($e->getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    static function finishGame(GameId $gameId): void {
+        GameService::finish($gameId);
+    }
+
+    static function joinSoloGame(string $name, GameId $gameId): bool {
+        try {
+            SoloGameService::join($name, $gameId);
+        } catch (\Exception $e) {
+            self::$logger->error($e->getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    static function joinTeamGame(string $name, GameId $gameId, ?TeamId $teamId = null, bool $force = false): bool {
+        try {
+            TeamGameService::join($name, $gameId, $teamId, $force);
+        } catch (\Exception $e) {
+            self::$logger->error($e->getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    //TeamGameのときのみ、チーム移動できるかModelで設定
+    static function moveTeam():bool {}
+    static function quitGame():bool {}
+
+    static function setSpawnPoint():bool{}
+    static function addTeamScore():bool{}
+    static function addPlayerScore():bool{}
+}
