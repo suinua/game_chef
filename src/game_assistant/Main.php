a8e8d1efb42315a4607956ad4f5d52c42fe21686
suinua <maatinosippo@gmail.com>
2021-04-05 00:19:04 +0900

    Join時とQuit時の処理を実装

---
+++ src/game_assistant/Main.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace game_assistant;
+
+
+use game_assistant\models\PlayerData;
+use game_assistant\store\PlayerDataStore;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener
+{
+    public function onEnable() {
+        GameAssistant::setLogger($this->getLogger());
+        GameAssistant::setScheduler($this->getScheduler());
+    }
+
+    public function onJoin(PlayerJoinEvent $event) {
+        try {
+            PlayerDataStore::add(new PlayerData($event->getPlayer()->getName()));
+        } catch (\Exception $e) {
+            $this->getLogger()->error($e->getMessage());
+            $event->setCancelled();
+        }
+    }
+
+    public function onQuit(PlayerQuitEvent $event) {
+        try {
+            PlayerDataStore::delete($event->getPlayer()->getName());
+        } catch (\Exception $e) {
+            $this->getLogger()->error($e->getMessage());
+            $event->setCancelled();
+        }
+    }
+}
