0f1d470de91e7c1b22ee2d967286fe028db8845b
suinua <maatinosippo@gmail.com>
2021-04-05 19:55:41 +0900

    LoadMapsの仮置

---
+++ src/game_assistant/utilities/LoadMaps.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace game_assistant\utilities;
+
+
+class LoadMaps
+{
+    static function byName(string $name): void { }
+
+    static function byGameType(): void { }
+
+    static function all(): void { }
+
+}
