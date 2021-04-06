8db762728ac3607da0b4f633453f36d09bf5b6ee
suinua <maatinosippo@gmail.com>
2021-04-06 19:14:14 +0900

    Eventを追加

--- src/game_chef/pmmp/events/AddedScoreEvent.php
+++ src/game_chef/pmmp/events/AddedScoreEvent.php
@@ -4,7 +4,38 @@
 namespace game_chef\pmmp\events;
 
 
-class AddedScoreEvent
-{
+use game_chef\models\GameId;
+use game_chef\models\Score;
+use game_chef\models\TeamId;
+use pocketmine\event\Event;
 
+class AddedScoreEvent extends Event
+{
+    private GameId $gameId;
+    private TeamId $teamId;
+    private Score $totalScore;
+    private Score $scoreAdded;
+
+    public function __construct(GameId $gameId, TeamId $teamId, Score $totalScore, Score $scoreAdded) {
+        $this->gameId = $gameId;
+        $this->teamId = $teamId;
+        $this->totalScore = $totalScore;
+        $this->scoreAdded = $scoreAdded;
+    }
+
+    public function getGameId(): GameId {
+        return $this->gameId;
+    }
+
+    public function getTeamId(): TeamId {
+        return $this->teamId;
+    }
+
+    public function getTotalScore(): Score {
+        return $this->totalScore;
+    }
+
+    public function getScoreAdded(): Score {
+        return $this->scoreAdded;
+    }
 }
