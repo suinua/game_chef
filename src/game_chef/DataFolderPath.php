58cab425f16940455c9a8fbba27b86e7375ad477
suinua <maatinosippo@gmail.com>
2021-04-07 03:10:35 +0900

    パスの追加

--- src/game_chef/DataFolderPath.php
+++ src/game_chef/DataFolderPath.php
@@ -6,9 +6,15 @@ namespace game_chef;
 
 class DataFolderPath
 {
-    static string $map;
+    static string $maps;
+    static string $teamGameMaps;
+    static string $ffaGameMaps;
+
     static function init(string $dataPath) {
-        self::$map = $dataPath . "maps/";
-        if (!file_exists(self::$map)) mkdir(self::$map);
+        self::$maps = $dataPath . "maps/";
+        if (!file_exists(self::$maps)) mkdir(self::$maps);
+
+        self::$teamGameMaps = self::$maps . "team_game_maps/";
+        self::$ffaGameMaps = self::$maps . "ffa_game_maps/";
     }
 }
