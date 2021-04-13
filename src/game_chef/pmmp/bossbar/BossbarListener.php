1c5f334fdebfabf732fa9004d58514c33502285b
suinua <maatinosippo@gmail.com>
2021-04-13 23:21:28 +0900

    ボスバーを実装

---
+++ src/game_chef/pmmp/bossbar/BossbarListener.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace game_chef\pmmp\bossbar;
+
+
+use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+
+class BossbarListener implements Listener
+{
+    public function onQuit(PlayerQuitEvent $event) {
+        $player = $event->getPlayer();
+        $bossBars = Bossbar::getBossbars($player);
+        foreach ($bossBars as $bossBar) {
+            $bossBar->remove();
+        }
+    }
+
+    public function onTeleport(EntityTeleportEvent $event) {
+        $player = $event->getEntity();
+        if ($player instanceof Player) {
+            $bossBars = BossBar::getBossbars($player);
+            foreach ($bossBars as $bossBar) {
+                $bossBar->updateLocationInformation($player);
+            }
+        }
+    }
+}
