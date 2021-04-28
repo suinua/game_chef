3ab335f8578d402eb61c0c45ba481692fb37f944
suinua <maatinosippo@gmail.com>
2021-04-28 23:26:51 +0900

    fix

--- src/game_chef/pmmp/form/team_game_map_forms/CustomTeamVectorDataListForm.php
+++ src/game_chef/pmmp/form/team_game_map_forms/CustomTeamVectorDataListForm.php
@@ -8,6 +8,7 @@ use form_builder\models\simple_form_elements\SimpleFormButton;
 use form_builder\models\SimpleForm;
 use game_chef\models\map_data\TeamDataOnMap;
 use game_chef\models\map_data\TeamGameMapData;
+use game_chef\pmmp\hotbar_menu\CustomTeamVectorDataHotbarMenu;
 use pocketmine\Player;
 
 class CustomTeamVectorDataListForm extends SimpleForm
@@ -23,12 +24,13 @@ class CustomTeamVectorDataListForm extends SimpleForm
                 }
             )
         ];
-        foreach ($teamGameMapData->getCustomMapVectorDataList() as $customMapVectorData) {
+        foreach ($teamDataOnMap->getCustomTeamVectorDataList() as $customTeamVectorData) {
             $buttons[] = new SimpleFormButton(
-                strval($customMapVectorData),
+                strval($customTeamVectorData->getVector3()),
                 null,
-                function (Player $player) {
-                    //todo:
+                function (Player $player) use ($customTeamVectorData, $teamGameMapData, $teamDataOnMap) {
+                    $menu = new CustomTeamVectorDataHotbarMenu($player, $teamGameMapData, $teamDataOnMap, $customTeamVectorData);
+                    $menu->send();
                 }
             );
         }
